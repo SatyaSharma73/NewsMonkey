9cf3295ba4dcc00da239adbcd2fe21fdb1a38709
SatyaSharma73 <satyasharma2964@gmail.com>
2023-06-28 12:13:22 +0530

    Initial Code

---
+++ src/components/News.js
@@ -0,0 +1,138 @@
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
+import PropTypes from "prop-types";
+
+export default class News extends Component {
+  static deafaultProps = {
+    country: "in",
+    pageSize: 8,
+    category: "general",
+  };
+
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  };
+
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+    };
+  }
+
+  async componentDidMount() {
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=b641ef266c844c2bbbd16bb03a13bc59&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(data);
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+      loading: false,
+    });
+  }
+
+  handleNext = async () => {
+    if (
+      this.state.page + 1 >
+      Math.ceil(this.state.totalResults / this.props.pageSize)
+    ) {
+      console.log("end");
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=${
+        this.props.country
+      }&category=${
+        this.props.category
+      }&apiKey=b641ef266c844c2bbbd16bb03a13bc59&pageSize=${
+        this.props.pageSize
+      }&page=${this.state.page + 1}`;
+      this.setState({ loading: true });
+      let data = await fetch(url);
+      let parsedData = await data.json();
+
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+        loading: false,
+      });
+    }
+  };
+
+  handleprev = async () => {
+    console.log("prev");
+    let url = `https://newsapi.org/v2/top-headlines?country=${
+      this.props.country
+    }&category=${
+      this.props.category
+    }&apiKey=b641ef266c844c2bbbd16bb03a13bc59&pageSize=${
+      this.props.pageSize
+    }&page=${this.state.page - 1}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+      loading: false,
+    });
+  };
+
+  render() {
+    return (
+      <div className="container my-3">
+        {/* This is a news component */}
+        <h2 className="text-center" style={{ margin: "35px 0px" }}>
+          NewsMonkey - Todays Top Headline
+        </h2>
+        {this.state.loading && <Spinner />}
+
+        <div className="row">
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <div className="col-md-6" key={element.url}>
+                  <NewsItem
+                    title={element.title ? element.title.split(".") : ""}
+                    des={
+                      element.description ? element.description.split(".") : ""
+                    }
+                    urlToImage={element.urlToImage}
+                    newsUrl={element.url}
+                  />
+                </div>
+              );
+            })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-danger"
+            onClick={this.handleprev}
+          >
+            {" "}
+            &larr; Previous
+          </button>
+          <button
+            disabled={
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pageSize)
+            }
+            type="button"
+            className="btn btn-warning"
+            onClick={this.handleNext}
+          >
+            Next &rarr;
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
